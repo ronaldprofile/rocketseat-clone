e175ce9a6171cc9adb53451b07688820793bbc22
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-04-02 21:50:27 -0300

    added route put, to update user data

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { GetUserController } from "./controllers/GetUserController";
+import { UpdatedUserController } from "./controllers/UpdatedUserControlled";
 
 const router = Router();
 
 router.post("/signup", new CreateUserController().handle);
 router.get("/user/:id", new GetUserController().handle);
-
+router.put("/user/:id", new UpdatedUserController().handle);
 export { router };
