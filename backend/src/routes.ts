86bf75264f3b0a421c9d1339f3cfea25311cbca1
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-04-14 23:20:18 -0300

    added route authenticate/login

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,28 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { GetUserController } from "./controllers/GetUserController";
+import { LoginUserController } from "./controllers/LoginUserController";
 import { UpdatedUserController } from "./controllers/UpdatedUserControlled";
 
 const router = Router();
 
+router.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+router.get("/github/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
 router.post("/signup", new CreateUserController().handle);
 router.get("/user/:id", new GetUserController().handle);
 router.put("/user/:id", new UpdatedUserController().handle);
+router.post("/authenticate", new AuthenticateUserController().handle);
+router.post("/authenticate/login", new LoginUserController().handle);
+
 export { router };
