7459ef5a5ed9b3963b4610703037d8d8a412c608
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-04-21 19:23:44 -0300

    submit form with react hook form

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,26 +1,63 @@
 import Image from "next/image";
-import { Button } from "../components/Button";
-import { Input } from "../components/Input";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import logo from "../assets/logo.svg";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { useAuth } from "../context/Auth";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
 import { EmailIcon } from "../assets/Email";
 import { LockIcon } from "../assets/Lock";
 import { EyesIcon } from "../assets/Eyes";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import { Container, Content, Wrapper, Form } from "../styles/pages/Login";
+
+import * as L from "../styles/pages/Login";
+
+interface LoginUserFormData {
+  email: string;
+  password: string;
+}
+
+const createUserLoginForm = yup.object().shape({
+  email: yup
+    .string()
+    .email("Insira um e-mail válido")
+    .required("Este campo é obrigatório"),
+  password: yup.string().required("Este campo é obrigatório")
+});
 
 export default function Login() {
+  const { signInWithEmailAndPassword } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting }
+  } = useForm({
+    resolver: yupResolver(createUserLoginForm)
+  });
+
+  const handleSubmitForm: SubmitHandler<LoginUserFormData> = async data => {
+    const { email, password } = data;
+
+    await signInWithEmailAndPassword(email, password);
+
+    reset();
+  };
+
   return (
-    <Container>
-      <Wrapper>
-        <Content>
+    <L.Container>
+      <L.Wrapper>
+        <L.Content>
           <div id="logo">
             <Image src={logo} alt="Logo da empresa Rocketseat" />
           </div>
           <h1>Faça seu login na plataforma</h1>
-        </Content>
+        </L.Content>
 
-        <Form>
+        <L.Form onSubmit={handleSubmit(handleSubmitForm)}>
           <section>
             <div className="input-group">
               <div className="field">
@@ -36,8 +73,13 @@ export default function Login() {
                     id="email"
                     name="email"
                     placeholder="E-mail"
+                    {...register("email")}
                   />
                 </div>
+
+                {errors.email && (
+                  <span className="error">{errors.email.message}</span>
+                )}
               </div>
             </div>
 
@@ -55,10 +97,15 @@ export default function Login() {
                     id="password"
                     name="password"
                     placeholder="Senha"
+                    {...register("password")}
                   />
 
                   <EyesIcon />
                 </div>
+
+                {errors.password && (
+                  <span className="error">{errors.password.message}</span>
+                )}
               </div>
             </div>
           </section>
@@ -67,8 +114,8 @@ export default function Login() {
             Esqueci minha senha
           </a>
 
-          <Button type="submit" variant="disabled">
-            entrar
+          <Button type="submit" variant="primary">
+            {isSubmitting ? "Carregando..." : "entrar"}
           </Button>
 
           <div className="register">
@@ -81,12 +128,12 @@ export default function Login() {
           <div className="login-github">
             <span>Ou entre com</span>
 
-            <a href="#">
+            <Button as="a" href="#">
               <GitHubLogoIcon /> Github
-            </a>
+            </Button>
           </div>
-        </Form>
-      </Wrapper>
-    </Container>
+        </L.Form>
+      </L.Wrapper>
+    </L.Container>
   );
 }
